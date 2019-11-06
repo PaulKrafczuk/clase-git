d196936726db8de377ff44ea2c26c16e011a643e
PaulKrafczuk <krafczukpaul@gmail.com>
2019-11-05 22:00:57 -0300

    codigo para ej 7

--- archivoFer3.js
+++ archivoFer3.js
@@ -1,1 +1,14 @@
 console.log("soy paul y escribo en el archivo 3 de fer");
+
+// lineas de codigo para ej 7, paul
+
+console.log('hola mundo');
+
+let nombres = ['pol', 'fer', 'maria', 'juan'];
+
+let curso = 'full stack con node'
+
+if(nombre[1]==fer){
+    console.log(curso);
+}
+//termina codigo escrito por paul
